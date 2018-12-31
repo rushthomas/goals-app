07ae5138b23355c194126d49e1a95b972ae7b65d
Thomas Rush <rush.thomas1@gmail.com>
2018-12-31 00:44:07 -0500

    removed res.send

--- routes/users-trial.js
+++ routes/users-trial.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/users-trial', (req, res) => {
   console.log("users trial worked")
-  res.send('users trial has worked')
+  res.end()
 })
 
 module.export = router
